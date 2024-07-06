2844f93a759bce6f603fb232f0d855afaadebd35
0102JohnnyTd <rt2824.recruit.1022@gmail.com>
2024-07-06 13:07:14 +0900

    Initial Commit

---
+++ SampleListAndForEach/SampleListAndForEachApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleListAndForEachApp.swift
+//  SampleListAndForEach
+//
+//  Created by Johnny Toda on 2024/07/06.
+//
+
+import SwiftUI
+
+@main
+struct SampleListAndForEachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
